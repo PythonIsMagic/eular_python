7798b415d36ff7ed95293ef42c0c8b360079c1f6
Erik Lunna <eslunna@gmail.com>
2016-03-29 22:00:01 -0500

    Refactored eular 1 and added unit tests.

--- eular1.py
+++ eular1.py
@@ -9,24 +9,51 @@ get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
 
+import unittest
 
-def main():
-    # Declare constants
-    UPPERLIMIT = 1000
-    MULTIPLE1 = 3
-    MULTIPLE2 = 5
 
-    # Initialize the sum variable
-    sum = 0
+def sum_multiples(factorlist, limit):
+    """
+    Returns the sum of the specificed multiples.
+    """
+
+    result = 0
 
     # Go through all numbers up to 1000
-    for i in range(UPPERLIMIT):
-        # If the current number is a multiple of 3 or 5, add it to the sum
-        if i % MULTIPLE1 == 0 or i % MULTIPLE2 == 0:
-            sum += i
+    for i in range(limit):
+        if is_multiple_of(i, factorlist):
+            result += i
 
-    # Display the sum of all the correct multiples.
-    print(sum)
+    return result
+
+
+def is_multiple_of(dividend, factorlist):
+    if factorlist == []:
+        raise ValueError()
+
+    for n in factorlist:
+        if dividend % n == 0:
+            return True
+    else:
+        return False
+
+
+class TestEular1(unittest.TestCase):
+    def test_ismultipleof_emptylist_throwException(self):
+        self.assertRaises(ValueError, is_multiple_of, 10, [])
+
+    def test_ismultipleof_1with1_ReturnTrue(self):
+        expected = True
+        result = is_multiple_of(1, [1])
+        self.assertEqual(expected, result)
+
+    def test_ismultipleof_2of4_ReturnTrue(self):
+        expected = True
+        result = is_multiple_of(4, [2])
+        self.assertEqual(expected, result)
 
 if __name__ == "__main__":
-    main()
+    factors = [3, 5]
+    UPTO = 1000
+    result = sum_multiples(factors, UPTO)
+    print('Sum of all multiples of 3 and 5 up to 1000 is {}'.format(result))
