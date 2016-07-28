2687e29e1da5a861cc8c8778e3f2398934f66386
Erik Lunna <eslunna@gmail.com>
2016-07-28 13:09:35 -0400

    Removed the faulty divisors_dividing function from factors.py. And removed the related tests.

--- factors.py
+++ factors.py
@@ -26,29 +26,3 @@ def divisors(n):
     # n will ALWAYS be a factor of itself.
     factors.add(n)
     return factors
-
-
-def divisors_dividing(n):
-    """
-    Finds all divisors of the target number and returns them as a set.
-    * NOTE!!!!! THIS FAILS TO FIND CERTAIN FACTORS
-    * EXAMPLE: FOR 24, it skips 8. only finds 1, 2, 3, 6, 12, 24
-    """
-    if n < 1:
-        raise ValueError('n must be a positive integer.')
-
-    factors = set()
-    factors.add(n)
-
-    i = 2
-    while i <= n:
-        if n % i == 0:
-            n = int(n / i)
-            factors.add(i)
-            factors.add(n)
-
-        else:
-            # only increment if we did not find a factor.
-            i = i + 1
-
-    return factors
