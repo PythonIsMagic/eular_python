2a4e8e754a32e8fe85fca5d0d818206884b8203f
Erik Lunna <eslunna@gmail.com>
2016-07-29 22:50:27 -0400

    Solved eular14: used a dictionary to keep results in for reuse.

--- eular14.py
+++ eular14.py
@@ -17,6 +17,8 @@ Eular Problem14
     NOTE: Once the chain starts the terms are allowed to go above one million.
 """
 
+import pdb
+
 
 def next_collatz(n):
     # To make the function more efficient, I will stay with the true definition of the problem
@@ -25,17 +27,50 @@ def next_collatz(n):
     if n < 1:
         raise ValueError('eular14.next_collatz: n must be a positive integer.!')
     elif n % 2 == 0:
-        return n / 2
+        return n // 2
     else:
         return (3 * n) + 1
 
 
-def collatz_seq(n):
+def collatz_seq(n, collatz_dict={}):
+    #  pdb.set_trace()
     if n < 1:
         raise ValueError('eular14.collatz_seq: n must be a positive integer.!')
 
+    #  print('\n****** COLLATZ SEQ for {}'.format(n))
     seq = [n]
     while n > 1:
         n = next_collatz(n)
-        seq.append(n)
+        if n in collatz_dict:
+            #  print('found shortcut in {}!'.format(n))
+            seq.extend(collatz_dict[n])
+            collatz_dict[seq[0]] = seq
+            #  print(seq)
+            return seq
+        else:
+            seq.append(n)
+
+    collatz_dict[seq[0]] = seq
+    print(seq)
     return seq
+
+
+def longest_collatz_seq(upto):
+    collatz_dict = {}
+    n = 0
+    n_len = 0
+
+    for i in range(1, upto):
+        seq = collatz_seq(i, collatz_dict)
+        if len(seq) > n_len:
+            n = i
+            n_len = len(seq)
+            print('New longest seq! {}, for a sequence {} steps long.'.format(n, n_len))
+    print('\n\n')
+    print('The longest collatz sequence under {} starts with the number {}'.format(upto, n))
+    print('which has a length of {}'.format(n_len))
+    print('')
+    print('The collatz_dict ended up with a length of {} entries.'.format(len(collatz_dict)))
+
+if __name__ == "__main__":
+    longest_collatz_seq(1000000)
