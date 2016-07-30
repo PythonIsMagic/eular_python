f690de2503d787103c9f355b01f34b7f7c8b8538
Erik Lunna <eslunna@gmail.com>
2016-07-29 21:59:00 -0400

    Filled in tests for eular14.next_collatz

--- test_eular14.py
+++ test_eular14.py
@@ -17,23 +17,48 @@ class TestEular14(unittest.TestCase):
     pass
     # Tests for next_collatz(n)
 
-    """
-    def test_nextcollatz_0_raiseException(self):
     def test_nextcollatz_neg1_raiseException(self):
+        self.assertRaises(ValueError, eular14.next_collatz, -1)
+
     def test_nextcollatz_0_raiseException(self):
+        self.assertRaises(ValueError, eular14.next_collatz, 0)
+
     def test_nextcollatz_1_returns1(self):
+        expected = 1
+        result = eular14.next_collatz(1)
+        self.assertEqual(expected, result)
+
     def test_nextcollatz_2_returns1(self):
+        expected = 1
+        result = eular14.next_collatz(2)
+        self.assertEqual(expected, result)
+
     def test_nextcollatz_3_returns10(self):
+        expected = 10
+        result = eular14.next_collatz(3)
+        self.assertEqual(expected, result)
+
     def test_nextcollatz_4_returns2(self):
+        expected = 2
+        result = eular14.next_collatz(4)
+        self.assertEqual(expected, result)
+
     def test_nextcollatz_5_returns16(self):
-    """
+        expected = 16
+        result = eular14.next_collatz(5)
+        self.assertEqual(expected, result)
 
     # Tests for collatz_seq(n):
     """
     def test_collatzseq_0_raiseException(self):
+
     def test_collatzseq_1_returns1(self):
+
     def test_collatzseq_2_returns2_1(self):
+
     def test_collatzseq_3_returns3_10_5_16_8_4_2_1(self):
+
     def test_collatzseq_4_returns4_2_1(self):
+
     def test_collatzseq_5_returns5_16_8_4_2_1(self):
     """
