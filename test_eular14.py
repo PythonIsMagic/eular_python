d0e791f61313f8db337d62efc7068f65485266c2
Erik Lunna <eslunna@gmail.com>
2016-07-29 21:55:58 -0400

    Added outlines for eular14 tests. next_collatz and collatz_seq.

---
+++ test_eular14.py
@@ -0,0 +1,39 @@
+import unittest
+import eular14
+
+"""
+    # Tests for
+    def test_(self):
+        self.assertRaises(ValueError)
+
+    def test(self):
+        expected =
+        result =
+        self.assertEqual(expected, result)
+"""
+
+
+class TestEular14(unittest.TestCase):
+    pass
+    # Tests for next_collatz(n)
+
+    """
+    def test_nextcollatz_0_raiseException(self):
+    def test_nextcollatz_neg1_raiseException(self):
+    def test_nextcollatz_0_raiseException(self):
+    def test_nextcollatz_1_returns1(self):
+    def test_nextcollatz_2_returns1(self):
+    def test_nextcollatz_3_returns10(self):
+    def test_nextcollatz_4_returns2(self):
+    def test_nextcollatz_5_returns16(self):
+    """
+
+    # Tests for collatz_seq(n):
+    """
+    def test_collatzseq_0_raiseException(self):
+    def test_collatzseq_1_returns1(self):
+    def test_collatzseq_2_returns2_1(self):
+    def test_collatzseq_3_returns3_10_5_16_8_4_2_1(self):
+    def test_collatzseq_4_returns4_2_1(self):
+    def test_collatzseq_5_returns5_16_8_4_2_1(self):
+    """
