58e9d3da29446452f686cabd77d1ed3b2afbd231
Erik Lunna <eslunna@gmail.com>
2016-08-11 14:30:03 -0400

    Added description of eular25.

---
+++ eular25.py
@@ -0,0 +1,13 @@
+"""
+++ Eular25
+
+*The Fibonacci sequence*
+    is defined by the recurrence relation:
+        F_n = F_n−1 + F_n−2, where F_1 = 1 and F_2 = 1.
+
+    The 12th term, F_12, is the first term to contain three digits.
+    What is the first term in the Fibonacci sequence to contain 1000 digits?
+"""
+
+if __name__ == "__main__":
+    print('Eular Problem 25:')
