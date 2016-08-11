f81cacb4c9c5bde756a863cb0f51b9297e7ed10d
Erik Lunna <eslunna@gmail.com>
2016-08-11 01:31:54 -0400

    Removed the matrix stuff from eular15, won't need it.

--- eular15.py
+++ eular15.py
@@ -11,41 +11,13 @@ the right and down, there are exactly 6 routes to the bottom right corner.
 How many such routes are there through a 20×20 grid?
 """
 
-
-# Assume a square matrix
-def set_paths(row, col, size):
-    paths = []
-    if row < size - 1:
-        paths.append('>')
-    if col < size - 1:
-        paths.append('V')
-    return paths
-
-
-def lattice_matrix(size):
-    return [[set_paths(row, col, size + 1) for row in range(size + 1)]
-            for col in range(size + 1)]
-
-
-def print_matrix(matrix):
-    _str = ''
-    for row in matrix:
-        for col in row:
-            _str += '{}'.format(str(col))
-        _str = _str.rstrip() + '\n'
-    return _str
-
-
 if __name__ == "__main__":
-    LENGTH = 5
-    matrix5 = lattice_matrix(5)
-    print(print_matrix(matrix5))
-
+    LENGTH = 3
     x, y = 0, 0
-    end = LENGTH
-    paths = []
+    routes = []
+    iterating = True
 
-    while x < LENGTH and y < LENGTH:
+    while iterating:
         # increase x first
 
         # increase y
