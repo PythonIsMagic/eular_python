7fc872f14cbb9d3abd7d8f2b3074b5cd6e7c1ba6
Erik Lunna <eslunna@gmail.com>
2016-08-11 03:51:15 -0400

    Created a version of eular15 that works for small grids.

--- eular15.py
+++ eular15.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# coding=utf-8
 
 """
 Eular15
@@ -11,15 +12,46 @@ the right and down, there are exactly 6 routes to the bottom right corner.
 How many such routes are there through a 20×20 grid?
 """
 
+#  while path[0] == (0, 0):
+
+
+def next_path(path, grid_len):
+    while len(path) > 0:
+        lastx, lasty = path.pop()  # Pop back one step
+        if len(path) == 0:
+            return False, []
+        x, y = path[-1]
+
+        if x == grid_len or y == grid_len:  # Wall. Only 1 choice available.
+            pass
+        elif lasty == y + 1:
+            pass
+        else:
+            path.append((x, y + 1))
+            return True, path
+    else:
+        return False, []
+
+
 if __name__ == "__main__":
-    LENGTH = 3
+    grid_len = 10
     x, y = 0, 0
-    routes = []
+    routes, path = [], []
     iterating = True
 
     while iterating:
-        # increase x first
+        path.append((x, y))
+        if x < grid_len:
+            x += 1
+        elif y < grid_len:
+            y += 1
+        else:
+            routes.append(path[:])  # We have reached the end node
+            iterating, path = next_path(path, grid_len)
+            if iterating:
+                x, y = path.pop()
 
-        # increase y
+    print('{} routes found!'.format(len(routes)))
 
-        pass
+    #  for r in routes:
+        #  print(r)
