9a83426260bf1d8ca38cd6f79ae471f658f988f7
Erik Lunna <eslunna@gmail.com>
2016-08-11 14:35:45 -0400

    Solved eular25 using the fibonacci generator from eular2.

--- eular25.py
+++ eular25.py
@@ -1,3 +1,4 @@
+# encoding=utf-8
 """
 ++ Eular25
 
@@ -9,5 +10,12 @@
     What is the first term in the Fibonacci sequence to contain 1000 digits?
 """
 
+import eular2
+
 if __name__ == "__main__":
     print('Eular Problem 25:')
+
+    for i, x in enumerate(eular2.fibonacci()):
+        if len(str(x)) >= 1000:
+            print('Term {} has over 1000 digits!'.format(i+1))
+            break
