54f001471af41c57349cf551a6a82237a2355dfb
Erik Lunna <eslunna@gmail.com>
2016-08-11 15:33:44 -0400

    Compress the sum function in eular23 to a list comp.

--- eular23.py
+++ eular23.py
@@ -29,12 +29,9 @@ if __name__ == "__main__":
     print('Eular project 25')
     LIMIT = 28123
     abundants = [i for i in range(1, LIMIT + 1) if factors.is_abundant(i)]
-    sums = set()
-
-    for i, a in enumerate(abundants):
-        for b in range(i, len(abundants)):
-            sums.add(a + abundants[b])
-
+    sums = set([a + abundants[b]
+                for i, a in enumerate(abundants)
+                for b in range(i, len(abundants))])
     unsummable = [i for i in range(LIMIT + 1) if i not in sums]
 
     print('Sum of all positive ints which can\'t be summed by 2 abundants={}'.format(
